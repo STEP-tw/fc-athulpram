7573eec5d13e6e57979714433506c95b40b45e07
praathul <44019979+athulpram@users.noreply.github.com>
2019-01-16 11:44:13 +0530

    Create request handler to handle the request to get files
    Created request handler to require files from server,
    Will show 404 error when the file is not found.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
+const fs = require("fs");
 const app = (req, res) => {
+  let path = req.url;
+  if (path == "/") {
+    path = "/index.html";
+  }
+  path = "." + path;
+  provideFileAsRes = provideFileContents.bind(this, res, path);
+  fs.exists(path, provideFileAsRes);
+};
+
+const provideFileContents = function(res, filePath, exists) {
+  if (exists) {
+    fs.readFile(filePath, (err,contents) => {
+      console.log(contents);
+
+      res.write(contents.toString());
+      res.end();
+    });
+    return;
+  }
   res.statusCode = 404;
   res.end();
 };
